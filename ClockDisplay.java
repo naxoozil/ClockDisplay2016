d866ae266360fd4780cad636fc32140be9c1ee81
naxoozil <nachoviuda@gmail.com>
2016-11-10 12:01:52 +0100

    0921 creada clase clock display , constructor y metodos fijar hora e incrementar hora

---
+++ ClockDisplay.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    private NumberDisplay horas;
+    
+    private NumberDisplay minutos;
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        horas = new NumberDisplay(24);
+        minutos = new NumberDisplay(60);
+    }
+    
+    public void fixHour(int hora1, int minutos1)
+    {
+		horas.setValue(hora1);
+		minutos.setValue(minutos1);
+    }
+
+	public void incrementMinutes()
+	{
+		minutos.increment();
+		if (minutos.getValue() == 0){
+			horas.increment();
+		}
+	}
+
+}
